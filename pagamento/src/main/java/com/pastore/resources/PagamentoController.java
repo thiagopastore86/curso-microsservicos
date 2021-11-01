aa9db7f1f6b61a35b59774e07e9d2e77d365ba54
Thiago Pastore <62858459+thiagopastore86@users.noreply.github.com>
2021-11-01 10:01:32 -0300

    adicionado Hystrix

--- pagamento/src/main/java/com/pastore/resources/PagamentoController.java
+++ pagamento/src/main/java/com/pastore/resources/PagamentoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.pastore.entities.Pagamento;
 import com.pastore.services.PagamentoService;
 
@@ -17,9 +18,15 @@ public class PagamentoController {
 	@Autowired
 	private PagamentoService service;
 	
+	@HystrixCommand(fallbackMethod = "getPagamentoAlternativo")
 	@GetMapping(value = "/{funcionarioId}/{dias}")
 	public ResponseEntity<Pagamento> getPagamento(@PathVariable Long funcionarioId, @PathVariable Integer dias){
 		Pagamento pag = service.getPagamento(funcionarioId, dias);
 		return ResponseEntity.ok(pag);
 	}
+	
+	public ResponseEntity<Pagamento> getPagamentoAlternativo(Long funcionarioId, Integer dias){
+		return ResponseEntity.ok(Pagamento.builder().nome("Não localizado").diasTrabalhados(0).rendaDiaria(0.0).build());
+	}
+		
 }
